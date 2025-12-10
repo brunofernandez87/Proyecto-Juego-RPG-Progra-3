6cb8242dbe8561631f4cdca41e08f24ee216d5c7
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-12-10 18:17:15 -0300

    Creacion de clase Orco

---
+++ src/Orco.java
@@ -0,0 +1,30 @@
+public class Orco extends Personaje{
+
+    public Orco(String nombre, String apodo, String fecha_de_nacimiento, Integer edad, Integer velocidad, Integer destreza, Integer fuerza, Integer nivel, Integer armadura) throws Exception {
+        super(nombre, apodo, fecha_de_nacimiento, edad, velocidad, destreza, fuerza, nivel, armadura);
+    }
+
+    @Override
+    public void atacar(Personaje enemigo) {
+        double danio= ((((valorAtaque()*efectividadDisparo())-enemigo.valorDefensa())/500)*100)*1.1;
+        if(danio <0){
+            danio=0.0;
+        }
+        int salud=enemigo.getSalud()-(int)danio;
+        enemigo.setSalud(salud);
+        System.out.println("Orco "+this.nombre+" ataca y causa "+(int)danio+" de daño a "+enemigo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return "Orco {" +
+                "nombre='" + nombre + '\'' +
+                ", apodo='" + apodo + '\'' +
+                ", salud=" + salud +
+                ", edad=" + edad +
+                ", fuerza=" + fuerza +
+                ", nivel=" + nivel +
+                ", armadura=" + armadura +
+                '}';
+    }
+}
